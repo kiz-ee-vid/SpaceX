b659a408d277b435c08e92988e0f2cc811329d47
Ilya <ilyakizeev@gmail.com>
2022-07-30 17:53:40 +0300

    added splash screen

--- app/src/main/java/com/example/spacex/MainActivity.kt
+++ app/src/main/java/com/example/spacex/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_SpaceX)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navView: BottomNavigationView = binding.navView
